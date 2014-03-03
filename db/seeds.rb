5475d18b358a6eb3a67cf569424a60f81e58459a
vagrant <vagrant@ironhack>
2014-03-03 17:15:22 +0000

    commit of file seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+	Proyect.create name: "NewIronHack 4", description: 'la leche'
+	Proyect.create name: "NewIronHack 5", description: 'muy cool'
+	Proyect.create name: "NewIronHack 6", description: 'mola mazo'
+	Proyect.create name: "NewIronHack 7", description: 'titanium'
